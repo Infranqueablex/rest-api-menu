f5df8c645b70e7c1b2a014b6dbc3360c1545c3d2
Infranx <robertsalazar548@gmail.com>
2026-04-23 22:23:42 -0400

    ACtualizacion de cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import routerMenu from './routers/comidas/routerMenu.js';
+const cors = require('cors');
+
 export default function ({ modeloMenu }) {
 
   const app = express()
 
   app.use(express.json());
-
+  app.use(cors({
+    origin: 'https://localhost:3000',
+    credentials: true
+  }));
   app.use('/api', routerMenu({ modeloMenu }))
   const PORT = process.env.PORT ?? 4000;
   app.use('/api/uploads', express.static('public/images'))
